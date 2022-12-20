2678f8e496e1cbd5a36d1a7c9edac3670f1c5629
Rawan Alharthi <s43800023@students.tu.edu.sa>
2022-12-20 10:18:50 +0300

    Initial Commit

---
+++ RGBColor/RGBColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBColorApp.swift
+//  RGBColor
+//
+//  Created by 라완 💕 on 26/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct RGBColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
