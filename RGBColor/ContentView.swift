ba6c21e6d4ff041d0e845381ccc002a8dd8e7e8e
Rawan Alharthi <s43800023@students.tu.edu.sa>
2023-02-27 03:33:07 +0300

    RGBColor Assignment

--- RGBColor/ContentView.swift
+++ RGBColor/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var RedValue: Double = 0.0
+    @State var GreenValue: Double = 0.0
+    @State var BlueValue: Double = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RGBcolor(RedValue: $RedValue, GreenValue: $GreenValue, BlueValue: $BlueValue)
         }
-        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RGBcolor: View {
+    
+    @Binding var RedValue: Double
+    @Binding var GreenValue: Double
+    @Binding var BlueValue: Double
+    
+    var body: some View {
+        VStack {
+            Text("Red:")
+            Slider(value: $RedValue, in: 0...1)
+                .accentColor(Color.red)
+            Text("Green:")
+            Slider(value: $GreenValue, in: 0...1)
+                .accentColor(Color.green)
+            Text("Blue:")
+            Slider(value: $BlueValue, in: 0...1)
+                .accentColor(Color.blue)
+            Text("Result:")
+            Rectangle()
+            .fill(Color(red: RedValue, green: GreenValue, blue: BlueValue))
+            .frame(width: 100, height: 100)
+        }
+        .padding(.horizontal)
     }
 }
